47342e462009539f57789a7d4893964d57a2e89a
Cristian Sima <cristian.sima93@yahoo.com>
2017-05-27 15:21:12 +0300

    fix #10

--- server/render.js
+++ server/render.js
@@ -10,10 +10,15 @@ const
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/media/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/media/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/media/favicon-16x16.png">
+        <link rel="manifest" href="/static/media/manifest.json">
+        <link rel="mask-icon" href="/static/media/safari-pinned-tab.svg" color="#ff0000">
+        <meta name="theme-color" content="#ffffff">
         <title>
           Live
         </title>
-        <!-- <link rel="icon" href="/static/logo.ico"> -->
       </head>
       <body>
         <div id="root"></div>
