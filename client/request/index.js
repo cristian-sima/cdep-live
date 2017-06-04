4e36b2dd8648c58613ed871807ba2d1f4154e04e
Cristian Sima <cristian.sima93@yahoo.com>
2017-06-04 23:22:54 +0300

    improve

--- client/request/index.js
+++ client/request/index.js
@@ -4,88 +4,58 @@ import agent from "superagent";
 
 import { withPromiseCallback, normalizeArray } from "utility";
 
-export const performLogin = (formData : any) => (
-  new Promise(
-    (resolve, reject) => (
-      agent.
-      post("/api/login").
-      send(formData).
-      type("form").
-      end(
-        withPromiseCallback(resolve, reject)
-      )
-    )
-  )
-);
+export const performLogin = (formData : any) => new Promise((resolve, reject) => (
+  agent.
+  post("/api/login").
+  send(formData).
+  type("form").
+  end(withPromiseCallback(resolve, reject))
+));
 
-export const updateUserList = () => (
-  new Promise((resolve, reject) => (
-    agent.
-    post("/api/users/update").
-    type("json").
-    end(
-      withPromiseCallback(
-        ({ Users }) => resolve(
-          normalizeArray(Users)
-        ),
-        reject
-      )
+export const updateUserList = () => new Promise((resolve, reject) => (
+  agent.
+  post("/api/users/update").
+  type("json").
+  end(
+    withPromiseCallback(
+      ({ Users }) => resolve(
+        normalizeArray(Users)
+      ),
+      reject
     )
   )
-)
-);
+));
 
-export const fetchUsers = () => (
-  new Promise((resolve, reject) => (
-    agent.
-    get("/api/users").
-    type("json").
-    end(
-      withPromiseCallback(
-        ({ Users }) => resolve(
-          normalizeArray(Users)
-        ),
-        reject
-      )
+export const fetchUsers = () => new Promise((resolve, reject) => (
+  agent.
+  get("/api/users").
+  type("json").
+  end(
+    withPromiseCallback(
+      ({ Users }) => resolve(
+        normalizeArray(Users)
+      ),
+      reject
     )
   )
-)
-);
+));
 
-export const changePassword = (formData : any) => (
-  new Promise(
-    (resolve, reject) => (
-      agent.
-      post("/api/auth/changePassword").
-      send(formData).
-      type("form").
-      end(
-        withPromiseCallback(resolve, reject)
-      )
-    )
-  )
-);
+export const changePassword = (formData : any) => new Promise((resolve, reject) => (
+  agent.
+  post("/api/auth/changePassword").
+  send(formData).
+  type("form").
+  end(withPromiseCallback(resolve, reject))
+));
 
-export const signOff = () => (
-  new Promise(
-    (resolve, reject) => (
-      agent.
-      post("/api/auth/signOff").
-      end(
-        withPromiseCallback(resolve, reject)
-      )
-    )
-  )
-);
+export const signOff = () => new Promise((resolve, reject) => (
+  agent.
+  post("/api/auth/signOff").
+  end(withPromiseCallback(resolve, reject))
+));
 
-export const performReconnect = () => (
-  new Promise(
-    (resolve, reject) => (
-      agent.
-      post("/api/auth/reconnect").
-      end(
-        withPromiseCallback(resolve, reject)
-      )
-    )
-  )
-);
+export const performReconnect = () => new Promise((resolve, reject) => (
+  agent.
+  post("/api/auth/reconnect").
+  end(withPromiseCallback(resolve, reject))
+));
--- client/types/actions.js
+++ client/types/actions.js
@@ -1,49 +1,29 @@
 /* eslint-disable no-use-before-define */
 // @flow
 
-export type Action = {|
-  type: 'SHOW_CAPTCHA';
-  payload: { id: string ; name : string },
-|} | {|
-  type: 'HIDE_CAPTCHA';
-  payload: string;
-|} | {|
-  type: 'ACCOUNT_CONNECTED';
-  payload: any;
-|} | {|
-  type: 'CHANGE_PASSWORD';
-|} | {|
-  type: 'CANCEL_SIGN_OFF';
-|} | {|
-  type: 'SIGN_OFF';
-  payload: any;
-|} | {|
-  type: 'RECONNECT';
-  payload: any;
-|} | {|
-  type: 'CONFIRM_SIGN_OFF';
-|} | {|
-  type: 'CONNECTING_LIVE';
-|} | {|
-  type: 'CONNECTED_LIVE';
-|} | {|
-  type: 'UPDATING_LIST';
-|} | {|
-  type: 'TOGGLE_ITEM';
-  payload: string;
-|} | {|
-  type: 'TOGGLE_PUBLIC_VOTE';
-|} | {|
-  type: 'UPDATE_USERS';
-  payload: any;
-|} | {|
-  type: 'FETCH_USERS';
-  payload: any;
-|} | {|
-  type: 'SHOW_BUTTONS';
-|}
+import type { State } from "./state";
 
-export type Dispatch = (action: Action | ThunkAction | PromiseAction | Array<Action>) => any;
-export type GetState = () => Object;
+export type Action =
+  { type: 'SHOW_CAPTCHA'; payload: { id: string ; name : string }; }
+| { type: 'HIDE_CAPTCHA'; payload: string; }
+| { type: 'ACCOUNT_CONNECTED'; payload: any; }
+| { type: 'CHANGE_PASSWORD'; }
+| { type: 'CANCEL_SIGN_OFF'; }
+| { type: 'SIGN_OFF'; payload: any; }
+| { type: 'RECONNECT'; payload: any; }
+| { type: 'CONFIRM_SIGN_OFF'; }
+| { type: 'CONNECTING_LIVE'; }
+| { type: 'CONNECTED_LIVE'; }
+
+| { type: 'UPDATING_LIST'; }
+| { type: 'TOGGLE_ITEM'; payload: string; }
+| { type: 'TOGGLE_PUBLIC_VOTE'; }
+
+| { type: 'UPDATE_USERS'; payload: any; }
+| { type: 'FETCH_USERS'; payload: any; }
+| { type: 'SHOW_BUTTONS'; }
+
+export type Dispatch = (action: Action | ThunkAction | PromiseAction) => any;
+export type GetState = () => State;
 export type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
 export type PromiseAction = Promise<Action>;
