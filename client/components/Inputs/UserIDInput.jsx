b430e7fdcb3e3fb35843ea59ac8b96af26c99b98
Cristian Sima <cristian.sima93@yahoo.com>
2017-06-10 14:47:42 +0300

    fix #49

--- client/components/Inputs/UserIDInput.jsx
+++ client/components/Inputs/UserIDInput.jsx
@@ -21,18 +21,42 @@ type DigitInputPropTypes = {
   };
 
   focusNext?: () => void;
+  focusPrevious?: () => void;
   onRegisterRef?: (callback : (node : any) => void) => void;
 }
+
+type OnKeyUp = (data : {
+  focusNext? : () => void;
+  focusPrevious? : () => void;
+}) => (event : KeyboardEvent) => void;
 
 import React from "react";
 
 import classnames from "classnames";
 import { Field } from "redux-form/immutable";
 
-const tryToFocusNext = (focusNext? : () => void) => (event) => {
+const tryToFocusNext : OnKeyUp = ({ focusNext, focusPrevious }) => (event) => {
   if (event.target.value && event.target.value.length === 1) {
-    if (typeof focusNext === "function") {
+    const
+      { key } = event,
+      { shiftKey } = event,
+      shouldFocusNext = (
+          !((key === "Tab" && shiftKey) || (key === "Shift" && !shiftKey))
+        );
+
+    if (shouldFocusNext && typeof focusNext === "function") {
       focusNext();
+    }
+  } else {
+    const
+      { key } = event,
+      shouldFocusPrevious = (
+          key === "Backspace" ||
+          key === "Delete"
+        );
+
+    if (shouldFocusPrevious && typeof focusPrevious === "function") {
+      focusPrevious();
     }
   }
 };
@@ -42,6 +66,7 @@ const DigitInput = ({
   autoFocus,
   onRegisterRef,
   focusNext,
+  focusPrevious,
   meta : { submitting },
 } : DigitInputPropTypes) => (
   <input
@@ -55,7 +80,10 @@ const DigitInput = ({
     max="9"
     maxLength={1}
     min="0"
-    onKeyUp={tryToFocusNext(focusNext)}
+    onKeyUp={tryToFocusNext({
+      focusPrevious,
+      focusNext,
+    })}
     pattern="[0-9]{1}"
     ref={onRegisterRef ? onRegisterRef : null}
     size="1"
@@ -72,6 +100,7 @@ class UserIDInput extends React.Component {
 
   focusPosition: (position: number) => () => void;
 
+  registerPosition1: () => void;
   registerPosition2: () => void;
   registerPosition3: () => void;
 
@@ -89,9 +118,13 @@ class UserIDInput extends React.Component {
         const field = positions[position];
 
         if (field && field !== null) {
-          field.focus();
+          field.select();
         }
       });
+    };
+
+    this.registerPosition1 = (node : any) => {
+      this.handleRegisterRef(1, node);
     };
 
     this.registerPosition2 = (node : any) => {
@@ -123,20 +156,23 @@ class UserIDInput extends React.Component {
         </label>
         <div className={`form-inline ${right ? right : "col-md-8"}`}>
           <Field
-            autoFocus
+             autoFocus
              component={DigitInput}
              focusNext={this.focusPosition(2)}
-              name="Position1"
+             name="Position1"
+             onRegisterRef={this.registerPosition1}
             />
           <Field
             component={DigitInput}
             focusNext={this.focusPosition(3)}
+            focusPrevious={this.focusPosition(1)}
             name="Position2"
             onRegisterRef={this.registerPosition2}
            />
           <Field
              component={DigitInput}
              focusNext={focusPassword}
+             focusPrevious={this.focusPosition(2)}
              name="Position3"
              onRegisterRef={this.registerPosition3}
             />
