e5e02be9ab893f2f9ba21bca3927ff0640031cfb
Cristian Sima <cristian.sima93@yahoo.com>
2017-06-13 04:11:54 +0300

    improve screen

--- client/components/Wall/CurrentItem.jsx
+++ client/components/Wall/CurrentItem.jsx
@@ -80,35 +80,32 @@ class CurrentItem extends React.Component {
       project = item.get("project"),
       position = item.get("position"),
       title = item.get("title"),
-      description = item.get("description"),
+      comment = item.get("comment"),
+      hasComment = typeof comment !== "undefined" && comment !== "",
       groupOption = item.get(group);
 
     return (
-      <div>
+      <div className="mt-lg-5">
         <div className="container">
           <div className="row">
-            <div className="col-sm-8">
-              <div className="h4 cursor-pointer" onClick={showItemDetails(id)}>
+            <div className="col">
+              <div className="display-4 cursor-pointer" onClick={showItemDetails(id)}>
                 <span className="badge badge-pill badge-info">{position}</span>
                 {" "}
                 <Optiune content={project} inline optiune={groupOption} />
               </div>
-              <div className="ellipsis-big">
-                {title}
+              <div className={`lead-truncate${hasComment ? "" : "-full"} pt-2 lead`}>
+                <div className={`ellipsis-big${hasComment ? "" : "-full"}`}>{title}</div>
               </div>
-              <div className="hidden-md-down">
-                {
-                  description ? (
-                    <div>
-                      <hr />
-                      {description}
-                    </div>
-                  ) : null
-                }
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className="mt-4 mt-md-0">
+              {
+                hasComment ? (
+                  <div className="font-italic lead-truncate lead">
+                    <hr />
+                    <div className="ellipsis-big">{comment}</div>
+                  </div>
+                ) : null
+              }
+              <div className="mt-4 mt-md-0 h1 mt-lg-3">
                 <Details data={item} group={group} />
               </div>
             </div>
